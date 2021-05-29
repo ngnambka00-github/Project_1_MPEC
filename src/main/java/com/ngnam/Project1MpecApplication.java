e5484424fe60f118a38d073f17c02ed9666c0713
ngnambka00-github <nam.nv182698@sis.hust.edu.vn>
2021-05-29 22:52:41 +0700

    first commit

---
+++ src/main/java/com/ngnam/Project1MpecApplication.java
@@ -0,0 +1,13 @@
+package com.ngnam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1MpecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project1MpecApplication.class, args);
+    }
+
+}
